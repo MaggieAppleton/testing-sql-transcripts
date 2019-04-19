e5057e5f9386b33531bc28efeeb708533e8f8f01
Maggie <maggie.fm.appleton@gmail.com>
2019-04-19 11:59:53 +0100

    Init commit

--- Link.js
+++ Link.js
@@ -1,18 +1,11 @@
 import React from 'react'
 
-const linkStyle = {
-  border: '1px solid #666',
-  borderRadius: '10px',
-  width: '300px',
-  padding: '20px',
-  textAlign: 'center',
-  margin: '0 auto',
-}
-
 const Link = props => {
   return (
-    <div style={linkStyle}>
-      <a href='{props.url}'>{props.urlname}</a>
+    <div class='fancylink'>
+      <a target='_blank' href='{props.url}'>
+        {props.urlname}
+      </a>
     </div>
   )
 }
